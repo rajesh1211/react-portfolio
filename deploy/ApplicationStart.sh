89f72bc8abeff8b6c0e3ff6ee82b203dadab0d71
Rajesh Raikwar <rajesh.raikwar1211@gmail.com>
2017-08-14 23:07:32 +0530

    edit deploy2

--- deploy/ApplicationStart.sh
+++ deploy/ApplicationStart.sh
@@ -3,5 +3,7 @@
 HOME="/home/ubuntu/react-portfolio"
 cd $HOME
 sudo killall node || true
+# sudo pm2 stop all
+sudo pm2 start server/index.js
 # sudo nohup node server/index.js &
 # sudo npm run prod-server
