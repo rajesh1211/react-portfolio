14aea7e6799af6d72b687f9e47b8fb9378c1c5e6
Rajesh Raikwar <rajesh.raikwar1211@gmail.com>
2017-08-27 01:24:08 +0530

    typeWiter effect

--- client/components/about.js
+++ client/components/about.js
@@ -9,10 +9,35 @@ import logo from '../images/mylogo.png'
 require('../styles/about.scss');
 
 class About extends React.Component {
-   constructor(props) {
+  constructor(props) {
     super(props)
     this.randomizeIcons = this.randomizeIcons.bind(this)
+
+    this.initTypewriter()
   }
+
+  initTypewriter() {
+    setTimeout( function() {
+      var salutationClass = document.getElementsByClassName('salutation')[0]
+      salutationClass.classList.add("remove-type-writer");
+      var houseNameClass = document.getElementsByClassName('house-name')[0]
+      houseNameClass.classList.add("typewriter");
+
+      setTimeout( function() {
+        houseNameClass.classList.add("remove-type-writer");
+        var description = document.getElementsByClassName('description')[0]
+        description.classList.add("typewriter");        
+
+        setTimeout( function() {
+          description.classList.add("remove-type-writer");
+          var descriptionEnd = document.getElementsByClassName('description-end')[0]
+          descriptionEnd.classList.add("typewriter");
+        }, 3500);  
+      }, 3500);
+    }, 3500);
+  }
+
+  
   randomizeIcons(e) {
     var clickedElementPosClass = e.target.className.match(/img-pos-[0-9]/g)[0]; 
     var index = parseInt(clickedElementPosClass[clickedElementPosClass.length -1])
@@ -42,12 +67,22 @@ class About extends React.Component {
           <div className="col-md-12 about-inner-container inner-container">
             
             <div className="intro">
-              <p className="typewriter salutation"> Hi!!, I am Rajesh.</p>
-              <p className="house-name">... from house Stark! &nbsp; </p>
+              <div className="typewriter-div">
+                <p className="typewriter salutation"> Hi!!, I am Rajesh.</p>
+              </div> 
+              <br/> 
+              <div className="typewriter-div">
+                <p className="house-name">... from house Stark! </p>
+              </div>  
 
               <div className="sub-intro">
-                <p className="description"> A Full Stack Developer by heart,  </p>
-                <p className="description"> which by the way is made of Valerian Steel. </p>
+                <div className="typewriter-div">
+                  <p className="description"> A Full Stack Developer by heart,  </p>
+                </div>
+                <br/>
+                <div className="typewriter-div">  
+                  <p className="description-end"> which by the way is made of Valerian Steel. </p>
+                </div>  
               </div>  
             </div> 
             
@@ -55,8 +90,6 @@ class About extends React.Component {
             <img src={darth} className='img-1 img-pos-1 about-images' onClick={this.randomizeIcons}/>
             <img src={console} className='img-2 img-pos-2 about-images' onClick={this.randomizeIcons}/>
             <img src={mac} className='img-3 img-pos-3 about-images' onClick={this.randomizeIcons}/>
-            
-            
 
           </div>
         </div>
